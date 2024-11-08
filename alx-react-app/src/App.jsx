51c4f3e30578341a31cb6bfb363beff180c9e078
Cannel Mabelane <98761910+Cannel4real@users.noreply.github.com>
2024-11-08 06:10:22 +0200

    user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> 
       <Footer />
     </>
   )
