a6df954f6c3f39d5d5bb0ea5306f83aaf7cf38d6
Cannel Mabelane <98761910+Cannel4real@users.noreply.github.com>
2024-11-12 04:07:01 +0200

    inline styles

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
     <footer>
-    <p>© 2023 City Lovers</p>
+    <p style={{ color: 'navy', padding: '10px', margin: '10px, 0px', fontSize: '15px' }}>© 2023 City Lovers</p>
     </footer>
     );
 }
