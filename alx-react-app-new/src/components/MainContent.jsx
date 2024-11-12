b3da6ed467e1a8ca17cbd6e8654b558e663e95ad
Cannel Mabelane <98761910+Cannel4real@users.noreply.github.com>
2024-11-12 04:06:06 +0200

    inline styles

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
     return(
         <main>
-        <p>I love to visit New York, Paris, and Tokyo.</p>
+        <p style={{ color: 'black', padding: '10px', margin: '10px, 0px', fontSize: '50px' }}>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
