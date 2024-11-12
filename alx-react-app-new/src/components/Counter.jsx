582443e569521a2740ad2436d09cb33e816251e6
Cannel Mabelane <98761910+Cannel4real@users.noreply.github.com>
2024-11-12 05:16:15 +0200

    counter

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,29 +1,15 @@
 import React from 'react'
 import react, {useState} from 'react';
 
-const Counter = () => {
-const [count, setCount] = useState(0);
+function Counter() {
+  const [count, setCount] = useState(0);
 
-    function increaseCounter() {
-        setCount(count + 1);
-    }
-    function decreaseCounter() {
-        setCount(count - 1);
-    }
-    function reset() {
-        setCount(0);
-    }
-    
-    
   return (
     <div>
-      <p style={{ color: 'black', padding: '5px', fontSize: '50px' }}>{count}</p>
-      <button style={{ color: 'white', fontSize: '30px', backgroundColor: '#0066b2', margin: '2px' }} onClick={increaseCounter}>increase</button> 
-      <button style={{ color: 'white', fontSize: '30px', backgroundColor: '#0066b2', margin: '2px' }} onClick={decreaseCounter}>Decrease</button> 
-      <button style={{ color: 'white', fontSize: '30px', backgroundColor: '#0066b2', margin: '2px' }} onClick={reset}>reset</button> 
-
+      <p>Current Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
-  )
+  );
 }
-
-export default Counter
