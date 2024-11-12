8e8c5cf59c3cbca8ef3ddeb88b91a57a692e1061
Cannel Mabelane <98761910+Cannel4real@users.noreply.github.com>
2024-11-12 06:54:07 +0200

    bug fix

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,6 +1,6 @@
-import React, { createContext } from 'react';
+import React, { useContext } from 'react';
 
 
-const UserContext = createContext();
+const UserContext = useContext();
 
 export default UserContext;
