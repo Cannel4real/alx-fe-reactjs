cb48b52ecce1e8b1265cb86474ac9a87838c9cfa
Cannel Mabelane <98761910+Cannel4real@users.noreply.github.com>
2024-12-02 05:05:45 +0200

    bug fix

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: 'class', // Default is 'media'. Use 'class' to toggle dark mode manually.
+
+  theme: {
+    extend: {}, // Extend the default theme (add custom styles here)
+  },
+
+  // Configure variants for your project
+  variants: {
+    extend: {
+      backgroundColor: ['active'], // Example: Add more utilities for specific states
+      textColor: ['visited'],
+    },
+  },
   theme: {
     extend: {},
   },
   plugins: [],
+  purge: {},
+
 }
 
