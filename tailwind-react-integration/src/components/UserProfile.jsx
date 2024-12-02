7f6b23c218ebd7f5660b524a2d2846cdd2cc0698
Cannel Mabelane <98761910+Cannel4real@users.noreply.github.com>
2024-12-02 12:05:06 +0200

    bug fix

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg sm:p-4 md:max-w-sm">
+      <div className="bg-gray-100 p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg sm:p-4 md:max-w-sm md:text-xl">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
